d99337f680d2f249e07aa0ae9295f28324dd7a59
Nicos Maris <nicos.maris@gmail.com>
2017-01-08 21:25:14 +0200

    minimal ui router

--- spec/test1.e2e.js
+++ spec/test1.e2e.js
@@ -1,50 +1,21 @@
 'use strict';
 
 describe("homepage", function() {
-      beforeEach(function() {
-          browser.get('/');
-      });
-
-      it('should expect something', function() {
-          var x = 1;
-          expect(x).toEqual(x);
-      });
-  });
-
-describe('calculator', function () {
-
-  beforeEach(module('calculatorApp'));
-
-  var $controller;
-
-  beforeEach(inject(function(_$controller_){
-    $controller = _$controller_;
-  }));
-
-  describe('sum', function () {
-    it('1 + 2 should equal 3', function () {
-      var $scope = {};
-      var controller = $controller('CalculatorController', { $scope: $scope });
-      $scope.x = 1;
-      $scope.y = 2;
-      $scope.sum();
-      expect($scope.z).toBe(3);
-    }); 
-
-    it('4 + -1 should equal 2', function () {
-      var $scope = {};
-      var controller = $controller('CalculatorController', { $scope: $scope });
-      $scope.x = 4;
-      $scope.y = -1;
-      $scope.sum();
-      expect($scope.z).toBe(3);
-    }); 
-
-    it('z should default to zero', function () {
-      var $scope = {};
-      var controller = $controller('CalculatorController', { $scope: $scope });
-      expect($scope.z).toBe(0);
+    beforeEach(function() {
+        browser.get('/');
     });
-  });
 
+    it('should expect something', function() {
+        var x = 1;
+        expect(x).toEqual(x);
+    });
+});
+
+describe('helloworld', function () {
+
+    beforeEach(module('helloworld'));
+    it('should expect something too', function() {
+        var x = 1;
+        expect(x).toEqual(x);
+    });
 });
