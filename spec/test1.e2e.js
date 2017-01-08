7d52a2823a2ec737007e7f177ff48eda814e45d0
Nicos Maris <nicos.maris@gmail.com>
2017-01-08 23:05:25 +0200

    .

--- spec/test1.e2e.js
+++ spec/test1.e2e.js
@@ -6,8 +6,19 @@ describe("homepage", function() {
     });
 
     it('should expect something', function() {
-        var x = 1;
-        expect(x).toEqual(x);
+var currentStateName = browser.executeAsyncScript(function(callback) {
+            var el = document.querySelector("html");  // ng-app is defined on html element in this case
+            var injector = angular.element(el).injector();
+            var service = injector.get('$state');
+
+            callback(service.current.name);
+        });
+
+        expect(currentStateName).toEqual("hello");
+          var hello_link = $('[href*=hello]');  
+          var about_link = $('[href*=about]');  
+          hello_link.click();
+          //expect(answer.getText()).toEqual("Chocolate!");
     });
 });
 
