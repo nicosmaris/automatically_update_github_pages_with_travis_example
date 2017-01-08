f83dfcb0b477fc78c124b5c87aec852b96388e82
Nicos Maris <nicos.maris@gmail.com>
2017-01-08 18:40:20 +0200

    port 4444

--- protractor.config.js
+++ protractor.config.js
@@ -13,7 +13,7 @@ var config = {
   capabilities: {
     'browserName': 'firefox' // or 'safari'
   },
-  baseUrl: 'http://localhost:8000',
+  baseUrl: 'http://localhost:4444/wd/hub',
   plugins: [{ inline: istanbulPlugin }]
 };
 
