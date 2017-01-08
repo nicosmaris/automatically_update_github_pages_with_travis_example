c6a22b99f3753eb95c6e6350cb7b92209d1385fa
Nicos Maris <nicos.maris@gmail.com>
2017-01-08 19:31:13 +0200

    trying to cover lines

--- test/test1.e2e.js
+++ test/test1.e2e.js
@@ -13,10 +13,3 @@ describe("homepage", function() {
       });
   });
 
-describe('Output the code coverage objects', function() {
-  it('should output the coverage object.', function() {
-    browser.driver.executeScript("return __coverage__;").then(function(val) {
-      fs.writeFileSync("coverage/e2e/coverageE2E.json", JSON.stringify(val));
-    });
-  });
-});
