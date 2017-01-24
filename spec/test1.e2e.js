6521f1a7f1bb1fde15f1ab4b5448e7a65313216c
Nicos Maris <nicos.maris@gmail.com>
2017-01-25 00:35:30 +0200

    remove coverage

--- spec/test1.e2e.js
+++ spec/test1.e2e.js
@@ -2,13 +2,13 @@
 
 describe("homepage", function() {
     beforeEach(function() {
-        browser.get('index.html#/');
+        browser.get('/index.html#/', 10000);
     });
 
     it('should redirect to hello when clicking on hello link', function() {
         expect(browser.getCurrentUrl()).toMatch('/');
-        var hello_link = $('[href*=hello]');  
-        var about_link = $('[href*=about]');  
+        var hello_link =  element(by.css('[ui-sref="hello"]'));
+        var about_link =  element(by.css('[ui-sref="about"]'));
         hello_link.click();
         expect(browser.getCurrentUrl()).toMatch('/#!/hello$');
     });
