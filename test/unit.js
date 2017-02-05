593c6d607764a6665fc2d655721c64179a0eed13
Nicos Maris <nicos.maris@gmail.com>
2017-02-05 02:15:30 +0200

    replaced angular with bare jquery

--- test/unit.js
+++ test/unit.js
@@ -1,17 +1,32 @@
 import test from 'ava';
 
+/* loads libs */
+var fs = require("fs");
 var jsdom = require("jsdom");
 
-jsdom.env({
-  url: "http://digg.com",
-  scripts: ["http://code.jquery.com/jquery.js"],
-  done: function (errors, window) {
-    var $ = window.$;
-    console.log($("a.story-title-link.story-link").text());
-  }
+/* loads HTML of page under test */
+var htmlfile = fs.readFileSync("index.html")
+
+/* does not load js handler of jquery ready event. jsdom.env is even more limited */
+var document = jsdom.jsdom(htmlfile, {
+  virtualConsole: jsdom.createVirtualConsole().sendTo(console)
 });
+var window = document.defaultView;
+window.onModulesLoaded = function () {
+  console.log("ready to roll!");
+};
+var $ = require('jquery')(window);
 
+/* copy js code here to avoid messing with commonJS and AMD or ES6 */
+function unit_under_test(){
+  $("#msgid").html("hello world");
+}
 
+/* jasmine tests */
 test('can add numbers', t => {
-	    t.is(1 + 1, 2);
+  unit_under_test()
+  console.log($('#msgid').html());
+  t.is(1 + 1, 2);
 });
+
+
